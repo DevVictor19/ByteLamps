57af7205beaf4adad82c091d56560d8be981bce2
Antonio Victor <Antoniovictor12@live.com>
2022-11-05 10:06:05 -0300

    feat: criar funcoes para lidar com os eventos

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 "use strict";
 
-const form = document.getElementById("main-form");
+const form = document.getElementById("decimal-form");
+const input = document.getElementById("decimal-input");
+const lights = document.querySelectorAll(".light");
+const bits = document.querySelectorAll(".bit");
 
-form.addEventListener("submit", (ev) => {
-  ev.preventDefault();
+form.addEventListener("submit", handleInput);
+input.addEventListener("input", handleInput);
 
-  const inputValue = Number(ev.target[0].value);
+function handleSubmit(event) {
+  event.preventDefault();
+  controlLights(parseByte(+event.target[0].value));
+}
 
-  console.log(parseByte(inputValue));
-});
+function handleInput(event) {
+  controlLights(parseByte(+event.target.value));
+}
+
+function controlLights(byteArr) {
+  byteArr.forEach((bit, index) => {
+    lights[index].classList.remove("--light-on");
+    bits[index].textContent = "0";
+
+    if (bit === 1) {
+      lights[index].classList.add("--light-on");
+      bits[index].textContent = "1";
+    }
+  });
+}
 
 function parseByte(decimal) {
   const result = [0, 0, 0, 0, 0, 0, 0, 0];
